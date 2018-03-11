0dd9f45ef55b6fc6c7e3cf7fee3de6adb390aa1a
Vcircuit <37259269+Vcircuit@users.noreply.github.com>
2018-03-11 11:31:34 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/Sourcecode', function (req, res) {
+    res.send('Vcircuit simulation requested and will be served here');
+}); 
